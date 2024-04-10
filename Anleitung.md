4b89ec866567e23ee232e0edee87e1e8a3b5627f
Jonas Schmid <schmid@puzzle.ch>
2024-04-10 14:07:35 +0200

    add new version

---
+++ Anleitung.md
@@ -0,0 +1,87 @@
+# Tic Tac Toe
+
+Wir programmieren zusammen ein Tic Tac Toe.
+
+Auf einem quadratischen, 3×3 Felder großen Spielfeld setzen die beiden Spieler abwechselnd ihr Zeichen (ein Spieler Kreuze, der andere Kreise) in ein freies Feld. Der Spieler, der als Erster drei Zeichen in eine Zeile, Spalte oder Diagonale setzen kann, gewinnt.
+
+# Planung
+
+## Wie viele Spieler hat das Spiel?
+Zwei Spieler
+
+## Welche Regeln hat das Spiel?
+* Die Spieler setzen abwechselnd ihr Zeichen auf ein freies Feld.
+
+## Welche Regeln beenden das Spiel?
+* Ein Spieler hat drei seiner Zeichen in eine Zeile, Spalte oder Diagonale gesetzen.
+* Es hat keine freien Felder.
+
+## Wie viele verschiedene Endzustände existieren?
+* Spieler X gewinnt
+* Spieler O gewinnt
+* Es ist unentschieden
+
+---
+
+# Umsetzung
+Wie können wir dies dem Computer beibringen?
+
+## Spielfeld zeichnen
+* Man könnte das Spielfeld als statischen HTML Code umsetzen.
+* Wir können das Spielfeld auch generieren:
+ 
+Funktion `render()`
+
+## Spielen
+* Die Spieler setzen abwechselnd ihr Zeichen in ein freies Feld.
+* Wir haben die Funktion handleCellClick(), welche beim Klick auf eine Feld ausgeführt wird. 
+
+Was müssen wir Programmieren?
+
+  1. Ist das Spiel zu Ende?
+  2. Ist die Zelle frei?
+  3. Das Setzen im Spielstand speichern.
+  4. Das Spielfeld aktualisieren
+  5. Überprüfen, ob das Spiel jetzt zu Ende ist
+    * → Den Spielstatus (gameState) setutem
+  6. Meldung aktualisieren
+
+## Spielende feststellen
+
+* Wie können wir feststellen, dass das Spiel zu Ende ist?
+  * Ein Spieler hat drei seiner Zeichen in eine Zeile, Spalte oder Diagonale gesetzen.
+  * Es hat keine freien Felder.
+* Wie viele Gewinnmöglichkeiten existieren?
+  * drei horizontal
+  * drei vertikal
+  * zwei diagonal
+
+```bash
+    possibleWinningPossibilities = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+    [0, 4, 8], [2, 4, 6]             // diagonal
+```
+
+* Die Funktion `checkForWinner()` überprüft, ob ein Spieler gewonnen hat oder das Spiel unentschieden ist, oder ob weitergespielt werden darf.
+
+Was müssen wir Programmieren?
+* Liegt eine Gewinnmöglichkeit vor?
+  * Ja → Wer ist der Sieger?
+* Falls nicht, hat es noch leere Felder?
+  * Ja → weiterspielen
+  * Nein → Unentschieden
+
+---
+// Spielstand: gameState
+// Brettstatus: boardState
+// aktueller Spieler: currentPlayer
+// message.textContent = "Test zum Anzeigen"
+
+    // 1) Ist das Spiel zu Ende
+    // 2) Hat keine freien Zellen
+    if () {
+        return;
+    }
+    // 3) Das Setzen im Spielstand speichern.
+
--- solution/script.js
+++ solution/script.js
@@ -1,69 +1,68 @@
 const board = document.getElementById('board');
 const message = document.getElementById('message');
+
+let gameState = 'playing' // playing, draw, X, O
 let currentPlayer = 'X';
+
 let boardState = ['', '', '', '', '', '', '', '', ''];
-let gameOver = false;
+const possibleWinningPossibilities = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+    [0, 4, 8], [2, 4, 6] // diagonals
+];
 
-function checkForWinner() {
-	console.log(boardState);
-	const possibleWinningPossibilities = [
-		[0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
-		[0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
-		[0, 4, 8], [2, 4, 6] // diagonals
-	];
-
-	for (const possibility of possibleWinningPossibilities) {
-		const a = possibility[0];
-		const b = possibility[1];
-		const c = possibility[2];
-
-		if (boardState[a] !== null) {
-			if (boardState[a] === boardState[b]) {
-				if (boardState[a] === boardState[c]) {
-					return boardState[a];
-				}
-			}
-		}
-		
-	}
-
-	if (!boardState.includes('')) {
-		return 'Draw';
-	}
-
-	return null;
+function render() {
+    board.innerHTML = '';
+    for (let i = 0; i < 9; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.textContent = boardState[i];
+        cell.addEventListener('click', () => handleCellClick(i));
+        board.appendChild(cell);
+    }
 }
 
 function handleCellClick(index) {
-	if (gameOver == true || boardState[index].length != 0) {
-		return;
-	}
+    if (gameState !== 'playing'
+        || boardState[index] !== '') {
+        return;
+    }
+    boardState[index] = currentPlayer;
+    render();
+    checkForWinner();
 
-	boardState[index] = currentPlayer;
-	render();
-	const winner = checkForWinner();
-	if (winner != null && winner.length != 0) {
-		if (winner === 'Draw') {
-			message.textContent = "Es ist unentschieden!";
-		} else {
-			message.textContent = `${winner} hat gewonnen!`;
-		}
-		gameOver = true;
-	} else {
-		currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-		message.textContent = `Spieler ${currentPlayer} ist an der Reihe`;
-	}
+    console.log('boardState:', boardState);
+    console.log('gameState:', gameState);
+
+    if (gameState === 'draw') {
+        message.textContent = "Es ist unentschieden!";
+    } else if (gameState === 'playing') {
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        message.textContent = `Spieler ${currentPlayer} ist an der Reihe`;
+    } else {
+        message.textContent = `Spieler ${gameState} hat gewonnen!`;
+    }
 }
 
-function render() {
-	board.innerHTML = '';
-	for (let i = 0; i < boardState.length; i++) {
-		const cell = document.createElement('div');
-		cell.classList.add('cell');
-		cell.textContent = boardState[i];
-		cell.addEventListener('click', () => handleCellClick(i));
-		board.appendChild(cell);
-	}
+function checkForWinner() {
+    for (const possibility of possibleWinningPossibilities) {
+        const symbol1 = boardState[possibility[0]];
+        const symbol2 = boardState[possibility[1]];
+        const symbol3 = boardState[possibility[2]];
+
+        if (symbol1 !== ''
+            && symbol1 === symbol2
+            && symbol1 === symbol3
+        ) {
+            gameState = symbol1;
+            return;
+        }
+    }
+
+    if (!boardState.includes('')) {
+        gameState = 'draw'
+    }
 }
 
 render();
+message.textContent = `Spieler ${currentPlayer} ist an der Reihe`;
