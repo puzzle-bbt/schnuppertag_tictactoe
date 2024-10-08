9623424b09765f2117a2f8b7d979d37c188d7801
Jannik Pulfer <pulfer@puzzle.ch>
2024-10-08 10:37:44 +0200

    Update comments of tasks

--- onlyMethodsGiven/script.js
+++ onlyMethodsGiven/script.js
@@ -23,38 +23,47 @@ function render() {
 }
 
 function handleCellClick(index) {
-    // Spielstand: gameState
+    // Spielstand: gameState | Mögliche Zustände: playing, draw, X, O
     // Brettstatus: boardState
-    // aktueller Spieler: currentPlayer
-    // message.textContent = "Test zum Anzeigen"
+    // Aktueller Spieler: currentPlayer | Mögliche Zustände: X, O
+    // message.textContent = "Text der angezeigt werden soll"
 
-    // 1) Ist das Spiel zu Ende
-    // 2) Hat keine freien Zellen
+    // Aufgabe 1) Ersetze true mit einer Überprüung ob das Spiel noch am laufen ist bzw. nicht zu Ende ist
+    // Aufgabe 2) Erweitere die Prüfung aus Aufgabe 1 und Prüfe ob die momentan angeklickte Zelle schon besetzt ist.
+    // Hinweis: Damit die Bedingung erfüllt ist, soll nur eine der beiden Konditionen zutreffen müssen.
     if (true) {
         return;
     }
 
-    // 3) Das Setzen des Spielers im Spielstand speichern.
+    // Aufgabe 3) Speichere den Zug des aktuellen Spielers an der korrekten Stelle im boardState array um den Spielstatus
+    // zu aktualisieren.
     // boardState[...] = ...
 
-    // 4) Das Spielfeld aktualisieren
+    // Aufgabe 4) Rufe die Funktion auf, welche das board für den Benutzer visuell aktualisiert, damit die Spieler feedback erhalten.
     //-->
 
-    // 5) Überprüfen ob das Spiel jetzt zu Ende ist -> Den Spielstatus (gameState) setzen.
+    // Aufgabe 5) Rufe die Funktion auf, welche überprüft ob ein Spieler gewonnen hat.
     //-->
 
-    // Zeigt der Status in der Browser-Console an.
+    // Zeigt den Status in der Browser-Konsole an.
     console.log('boardState:', boardState);
     console.log('gameState:', gameState);
 
-    //Spielstand (gameState) überprüfen und jeweilige message setzen
-    //draw ("Es ist unentschieden")
+    // Aufgabe 9) Setze die Nachricht die den Spielern angezeigt wird falls das Spiel zu Ende ist.
+
+    // Ersetze hier true durch eine Überprüfung ob der gameState unentschieden (draw) ist.
+    // Wenn ja, setze die Nachricht auf: "Es ist unentschieden!"
     if (true) {
-    //playing ("Spieler ... ist an der Reihe")
+
+    // Ersetze hier true durch eine Überprüfung ob der gameState sagt, dass das Spiel noch läuft.
+    // Wenn ja, setze die Nachricht nach dem Spielerwechsel auf "Spieler ... ist an der Reihe". Ersetze ... durch den Spieler
+    // der auch wirklich an der Reihe ist.
     } else if (true) {
         //Wechselt Spieler
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    //Spieler hat gewonnen ("Spieler ... hat gewonnen")
+
+    // Wenn oben beides nicht zutrifft, dann können wir davon ausgehen, dass ein Spieler gewonnen hat. Lies den Gewinner aus dem
+    // gameState aus und zeige die Nachricht "Spieler ... hat gewonnen!" an. Ersetze ... durch den Spieler der gewonnen hat.
     } else {
     }
 }
@@ -62,21 +71,25 @@ function handleCellClick(index) {
 function checkForWinner() {
     //Durch Gewinnmöglichkeiten loopen
     for (const possibility of possibleWinningPossibilities) {
-        //Möglichkeiten auslesen
-        // const symbol1 = ...
-        // const symbol2 = ...
-        // const symbol3 = ...
+        // Aufgabe 6) possibility ist pro durchgang des loops jeweils einer der Einträge im possibleWinningPossibilities
+        // Array. Lese nun aus dem boardState die drei Symbole aus, die sich bei den drei Positionen der momentanen possibilty
+        // befinden. Denke daran: possibilty ist auch ein array.
 
-        //Symbol ist nicht leer und alle Symbole sind gleich
+        // const symbol1 = boardState[...]
+        // const symbol2 = boardState[...]
+        // const symbol3 = boardState[...]
+
+        // Aufgabe 7) Überprüfe nun ob jemand gewonnen hat. Ersetze dafür true durch eine Überprüfung ob die Symbole
+        // nicht leer ('') sind und ob alle drei Symbole die Gleichen sind.
         if (true) {
-            //Gewinner im Spielstatus setzen
-            //-->
+            // Aufgabe 8) Speichere das Symbol des Gewinners im gameState
+            // ->
             return;
         }
     }
 
-    //Wenn der Brettstatus kein leeres Feld mehr beinhaltet, den Spielstatus auf untentschieden setzen
-    if (true) {
+    if (!boardState.includes('')) {
+        gameState = 'draw'
     }
 }
 
